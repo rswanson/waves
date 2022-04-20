9064eb90d67aa8d7e2fad8f54cc94d3cca2046ee
Ryan Swanson <ryan.s.swanson@gmail.com>
2022-04-20 18:32:51 -0400

    updating run to exercise new contract functions

--- scripts/run.js
+++ scripts/run.js
@@ -1,9 +1,22 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable no-process-exit */
 const main = async () => {
+  const [owner, randomPerson] = await hre.ethers.getSigners();
+  // eslint-disable-next-line no-undef
   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
   const waveContract = await waveContractFactory.deploy();
   await waveContract.deployed();
+
   console.log("Contract deployed to: ", waveContract.address);
+  console.log("Contract deployed by:", owner.address);
+
+  let waveCount;
+  waveCount = await waveContract.getTotalWaves();
+
+  const waveTxn = await waveContract.wave();
+  await waveTxn.wait();
+
+  waveCount = await waveContract.getTotalWaves();
 };
 
 const runMain = async () => {
